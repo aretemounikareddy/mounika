26bd389365adbf44ad768fdb652d5e094d2afbd5
Mounika <mounika.arete@sonata-software.com>
2019-08-14 11:09:58 +0530

    Hii!! This is commited

---
+++ AverageNumbers.java
@@ -0,0 +1,22 @@
+package com.sonata.Training;
+
+public class AverageNumbers {
+	public static void main(String[] args) {
+        //declares the three numbers
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 45;
+        
+        //get the average of the three numbers
+        // and saves it inside the ave variable
+        int ave = (num1+num2+num3)/3;
+        
+        //prints the output on the screen
+        System.out.println("number 1 = "+num1);
+        System.out.println("number 2 = "+num2);
+        System.out.println("number 3 = "+num3);
+        System.out.println("Average is = "+ave);
+    }
+    
+
+}
